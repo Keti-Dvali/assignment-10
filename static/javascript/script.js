ca47c89af8fcef6e58e73afa1e7cf9ba6d4aa3b2
Keti-Dvali <104909714+Keti-Dvali@users.noreply.github.com>
2022-09-02 13:48:15 -0700

    update style.css

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -4,5 +4,7 @@ let person = {
     age: 23, 
     isMarried: false, 
     webLanguages: ['html', 'css'],
+    profession: 'International Relations', 
+    graduationDate: 2021,
 }
-console.log(person.firstName, person.lastName, person.age, person.isMarried, person.webLanguages[0], person.webLanguages[1]);
+console.log(person.firstName, person.lastName, person.age, person.isMarried, person.webLanguages[0], person.webLanguages[1], person.profession, person.graduationDate);
