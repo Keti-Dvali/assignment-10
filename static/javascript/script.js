581c80e695c3a299d0344f81e67440b218c70be9
Keti-Dvali <104909714+Keti-Dvali@users.noreply.github.com>
2022-09-01 17:41:37 -0700

    need some improvements

---
+++ static/javascript/script.js
@@ -0,0 +1,8 @@
+let person = {
+    firstName: 'Keti',
+    lastName: 'Dvali',
+    age: 23, 
+    isMarried: false, 
+    webLanguages: ['html', 'css'],
+}
+console.log(person.firstName, person.lastName, person.age, person.isMarried, person.webLanguages[0], person.webLanguages[1]);
